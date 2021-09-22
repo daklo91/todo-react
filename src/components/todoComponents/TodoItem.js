4a90618284f3d8e53fb2ad1449fe17e1aae0f58f
daklo91 <coffeedog91@gmail.com>
2021-09-22 17:53:25 +0200

    Update SVG

--- src/components/todoComponents/TodoItem.js
+++ src/components/todoComponents/TodoItem.js
@@ -4,16 +4,27 @@ function TodoItem() {
   return (
     <li>
       <div className={classes.groupCheckmarkText}>
-        <div className={classes.checkmark}></div>
+        <button>
+          <div className={classes.checkmark}></div>
+        </button>
         <div className={classes.text}>Lorem ipsum dolor</div>
       </div>
-      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18">
-        <path
-          fill="#494C6B"
-          fill-rule="evenodd"
-          d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"
-        />
-      </svg>
+      <button className={classes.svgButton}>
+        <svg
+          width="12"
+          height="12"
+          viewBox="0 0 12 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M11.7851 0.471404L11.3137 0L5.89256 5.42115L0.471404 0L0 0.471404L5.42115 5.89256L0 11.3137L0.471404 11.7851L5.89256 6.36396L11.3137 11.7851L11.7851 11.3137L6.36396 5.89256L11.7851 0.471404Z"
+            fill="#494C6B"
+          />
+        </svg>
+      </button>
     </li>
   );
 }
