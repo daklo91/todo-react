4d8655df5cf341554d553749b083f5fd82f3500a
daklo91 <coffeedog91@gmail.com>
2021-09-22 17:54:16 +0200

    Add watcher for dark mode change

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import MainHeader from "./components/layout/MainHeader";
 import TodoList from "./components/todoComponents/TodoList";
 import "./App.css";
 
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", function (e) {
+    console.log("changed!!");
+  });
+
 function App() {
   return (
     <div>
