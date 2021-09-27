261454b12731c31e9f4c554545b534626cee6c8e
daklo91 <coffeedog91@gmail.com>
2021-09-27 16:25:49 +0200

    Add control buttons Add style to control buttons

--- src/components/todoComponents/ClearTodo.js
+++ src/components/todoComponents/ClearTodo.js
@@ -4,7 +4,12 @@ function ClearTodo() {
   return (
     <div className={classes.container}>
       <span>0 items left</span>
-      <button>Clear Completed</button>
+      <div className={classes.controlDiv}>
+        <button className={classes.controlButton}>All</button>
+        <button className={classes.controlButton}>Active</button>
+        <button className={classes.controlButton}>Completed</button>
+      </div>
+      <button className={classes.clearButton}>Clear Completed</button>
     </div>
   );
 }
