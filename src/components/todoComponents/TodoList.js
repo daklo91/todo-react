4d760581df12d47864cafa6ab56904d568ed3d16
daklo91 <coffeedog91@gmail.com>
2021-09-30 14:58:46 +0200

    Handle state pipeline

--- src/components/todoComponents/TodoList.js
+++ src/components/todoComponents/TodoList.js
@@ -3,11 +3,21 @@ import ClearTodo from "./ClearTodo";
 import classes from "./TodoList.module.css";
 
 function TodoList(props) {
+  function handleCompleteToggle(id) {
+    props.handleCompleteToggle(id);
+  }
+
   return (
     <div className={classes.container}>
       <ul>
         {props.todoListData.map((todo) => (
-          <TodoItem key={todo.id} text={todo.text} />
+          <TodoItem
+            key={todo.id}
+            text={todo.text}
+            completeToggle={handleCompleteToggle}
+            id={todo.id}
+            complete={todo.complete}
+          />
         ))}
       </ul>
       <ClearTodo />
